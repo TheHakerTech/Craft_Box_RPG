bdcbb5eb934198b3949bac66c5b26e38a57d6527
Kotaz <semechkagent@gmail.com>
2022-12-05 17:42:38 +0300

    Избавление от зависимостей

--- colors.py
+++ colors.py
@@ -1,44 +1,76 @@
 # -*- coding: utf-8 -*-
-import colorama
-from colorama import Fore, Back, Style
+import os; os.system('') # for windows
+
 """
 Модуль цветов. Используй документацию класса TColors
+
+Принцип: для смена цвета используется ESC-последовательность, которая записывается так:
+\033[XXm
+
+\033[0m - сброс цвета
+\033[1m - жирный (яркий) шрифт
+
+3x - foreground
+4x - background
+x = 0 - black
+x = 1 - red
+x = 2 - green
+x = 3 - yellow
+x = 4 - blue
+x = 5 - magenta
+x = 6 - cyan
+x = 7 - white
 """
-colorama.init()
+
 class TColors:
     """
-    Класс для цветов
-    Используйте TColors.red(<текст>) для получения красного текста
-    Используйте TColors.redb(<текст>) для получения красного фона
-    Список поддерживаемых цветов текста:
-    -red
-    -blue
-    -green
-    -black
-    -white
-    -yellow
-    -pink
-    Список поддерживаемых цветов фона:
-    -redb
-    -blueb
-    -greenb
-    -blackb
-    -whiteb
-    -yellowb
-    -pinkb
+    Пример использования:
+    >>> from colors import TColors
+    >>> print(TColors.red('Hello, world!')) # красный текст
+    >>> print(TColors.yellowb('Hello, world!')) # желтый фон
     """
-    red = lambda text: Fore.RED + str(text) + Style.RESET_ALL
-    redb = lambda text: Back.RED + str(text) + Style.RESET_ALL
-    blue = lambda text: Fore.BLUE + str(text) + Style.RESET_ALL
-    blueb = lambda text: Back.BLUE + str(text) + Style.RESET_ALL
-    green = lambda text: Fore.GREEN + str(text) + Style.RESET_ALL
-    greenb = lambda text: Back.GREEN + str(text) + Style.RESET_ALL
-    black = lambda text: Fore.BLACK + str(text) + Style.RESET_ALL
-    blackb = lambda text: Back.BLACK + str(text) + Style.RESET_ALL
-    white = lambda text: Fore.WHITE + str(text) + Style.RESET_ALL
-    whiteb = lambda text: Back.WHITE + str(text) + Style.RESET_ALL
-    yellow = lambda text: Fore.YELLOW + str(text) + Style.RESET_ALL
-    yellowb = lambda text: Back.YELLOW + str(text) + Style.RESET_ALL
-    pink = lambda text: Fore.PINK + str(text) + Style.RESET_ALL
-    pinkb = lambda text: Back.PINK + str(text) + Style.RESET_ALL
-    br = lambda text: Style.BRIGHT + str(text) + Style.RESET_ALL
+    @staticmethod
+    def clr(code):
+        return f'\033[{code}m'
+    @staticmethod
+    def wrap(text, code):
+        return TColors.clr(code) + str(text) + TColors.clr(0)
+    
+    def black(self, text):
+        return self.wrap(text, 30)
+    def red(self, text):
+        return self.wrap(text, 31)
+    def blue(self, text):
+        return self.wrap(text, 32)
+    def green(self, text):
+        return self.wrap(text, 33)
+    def black(self, text):
+        return self.wrap(text, 34)
+    def white(self, text):
+        return self.wrap(text, 35)
+    def yellow(self, text):
+        return self.wrap(text, 36)
+    def pink(self, text):
+        return self.wrap(text, 37)
+    
+    def blackb(self, text):
+        return self.wrap(text, 0)
+    def redb(self, text):
+        return self.wrap(text, 1)
+    def blueb(self, text):
+            return self.wrap(text, 2)
+    def greenb(self, text):
+            return self.wrap(text, 3)
+    def blackb(self, text):
+            return self.wrap(text, 4)
+    def whiteb(self, text):
+            return self.wrap(text, 5)
+    def yellowb(self, text):
+            return self.wrap(text, 6)
+    def pinkb(self, text):
+            return self.wrap(text, 7)
+        
+    def br(self, text):
+        return self.wrap(text, 1)
+    
+    
