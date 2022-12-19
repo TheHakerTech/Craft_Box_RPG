b27736d5dd09301fb99811b27d8ed08b3e632f5a
hopTech <aller_top@mail.ru>
2022-12-19 16:37:24 +0700

    Убрал лишнее

--- libs/_map.py
+++ libs/_map.py
@@ -9,52 +9,6 @@ def set_map(location, total_loc_name):
     rec_walk_locs(location, _map)
     return _map
 
-class Location:
-    def __init__(
-        self, name: str, entities: dict, hardlevel: int, description, underlocs=dict()
-    ):
-        """
-        Busic class for locations. Parameters:
-        -`name` str location name
-        -`hardlevel` int location hardlevel
-        -`description` location description
-        All parameters are requared
-        """
-        self.name = name.lower()
-        self.underlocs = underlocs
-        self.entities = entities
-
-        self.ent_names = {str(k): v for (k, v) in enumerate(self.entities.values(), 1)}
-        self.names = {str(k): v for (k, v) in enumerate(self.underlocs.values(), 1)}
-
-        self.hardlevel = hardlevel
-        self.description = description
-
-    def doc(self):
-        return str(self.description)
-
-        
-shop = Location(
-    "Магазин",
-    {},
-    2,
-    "Магазин полезных вещей",
-)
-tram = Location(
-    "Трамвай", {}, 1, "Трамвайная дорога проложенная с давних времён..."
-)
-tram_station = Location(
-    "Трамвайная станция", {}, 3, "", underlocs={tram.name: tram}
-)
-
-the_crossroads = Location(
-    "Перепутье",
-    {},
-    1,
-    "Подземная дорога что ведёт в глубины королевства...",
-    underlocs={shop.name: shop, tram_station.name: tram_station},
-)
-
 def rec_walk_locs(location, map_):
     global total_loc_name
     if   location.hardlevel == 1:
